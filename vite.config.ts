dc077f423056bdfbfe2ea11305bb3f0d114b6d9f
yuliyulidaz <yulinoapple@gmail.com>
2025-12-21 12:12:52 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Electron 빌드 시 상대 경로가 필수입니다.
   build: {
     outDir: 'dist',
     emptyOutDir: true,
